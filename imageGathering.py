5ec41e810a4eed3979efb1101124d3da66997759
medinkr1 <kevin.medina.santiago@jhuapl.edu>
2021-12-08 22:45:54 -0500

    Edited the script to include pre run arguments, or ask for them during run.

--- imageGathering.py
+++ imageGathering.py
@@ -1,7 +1,26 @@
 import flickr_scraper
+import argparse
 
 if __name__ == '__main__':
-    search_term = input("Search term: ")
-    image_amount = int(input("Download amount: "))
-    download_dir = input("Download Directory: ")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--search', type=str, help='flickr search term') 
+    parser.add_argument('-n', type=int, help='number of images')
+    parser.add_argument('-d', type=str)
+    opt = parser.parse_args()
+
+    if(opt.search == None):
+        search_term = input("Search term: ")
+    else:
+        search_term = opt.search
+
+    if(opt.n == None):
+        image_amount = input("Download amount: ")
+    else:
+        image_amount = opt.n
+
+    if(opt.d == None):
+        download_dir = input("Download Directory: ")
+    else:
+        download_dir = opt.d
+    
     flickr_scraper.get_urls(search_term, download_dir, image_amount, True)
